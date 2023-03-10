1dd6857e26807591ce92cd30174e44616470b2d5
ksgsslee <71924123+ksgsslee@users.noreply.github.com>
2023-03-11 00:32:09 +0900

    팔로잉 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import status
+
 from .serializer import SignupSerializer, SuggestionUserSerializer
 
 
@@ -16,3 +20,32 @@ class SignupView(CreateAPIView):
 class SuggestionList(ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = SuggestionUserSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset().exclude(pk=self.request.user.pk).exclude(pk__in=self.request.user.following_set.all())
+
+        return qs
+
+
+@api_view(["POST"])
+def user_follow(request):
+    username = request.data['username']
+
+    current_user = request.user
+    follow_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    current_user.following_set.add(follow_user)
+    follow_user.follower_set.add(current_user)
+
+    return Response(status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["POST"])
+def user_unfollow(request):
+    username = request.data['username']
+
+    current_user = request.user
+    follow_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    current_user.following_set.remove(follow_user)
+    follow_user.follower_set.add(current_user)
+
+    return Response(status.HTTP_204_NO_CONTENT)
