6f02443c1354204e6a30dae0c9990f3413709add
ksgsslee <71924123+ksgsslee@users.noreply.github.com>
2023-03-11 23:39:44 +0900

    좋아요 기능 추가

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from django.db.models import Q
 
 
@@ -23,3 +26,20 @@ class PostViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         return super().perform_create(serializer)
+
+    @action(detail=True, methods=["POST"])
+    def like(self, request, pk):
+        post = self.get_object()
+        post.like_user_set.add(self.request.user)
+        return Response(status.HTTP_201_CREATED)
+
+    @like.mapping.delete
+    def unlike(self, request, pk):
+        post = self.get_object()
+        post.like_user_set.remove(self.request.user)
+        return Response(status.HTTP_204_NO_CONTENT)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
